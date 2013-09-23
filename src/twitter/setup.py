80c692ec1a8df9366ea8562838e6e819b0f2f175
Priska Herger <priska@23bit.net>
2013-09-23 19:01:03 +0200

    fixed indentation.

--- src/twitter/setup.py
+++ src/twitter/setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(
-	name='get-user-timeline',
+    name='get-user-timeline',
     version='0.1',
     description='Retrieve Twitter user timeline and save tweets to file.',
     install_requires=[ 'argparse', 'oauth2' ]
