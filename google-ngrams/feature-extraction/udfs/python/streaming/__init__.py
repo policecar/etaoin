44271a8c1790940a1e7d20b8a640b592d4f5c322
Priska Herger <priska@23bit.net>
2013-10-17 21:30:04 +0200

    added python streaming module for local development and testing.

---
+++ google-ngrams/feature-extraction/udfs/python/streaming/__init__.py
@@ -0,0 +1,4 @@
+# this module is copied from https://svn.apache.org/repos/asf/pig/trunk/src/python/streaming/
+# and placed here to facilitate local development and testing
+
+__all__ = [ "controller", "pig_util" ]
