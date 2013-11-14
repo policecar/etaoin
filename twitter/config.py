deed26bc5229298ce0f5bf630de16e767304a7b6
Priska Herger <priska@23bit.net>
2013-11-14 16:59:01 +0100

    [minor] converted indentation to spaces.

--- twitter/config.py
+++ twitter/config.py
@@ -4,10 +4,10 @@ import os
 
 # Twitter app credentials
 # ( cf. https://dev.twitter.com/ )
-CONSUMER_KEY	= ""
-CONSUMER_SECRET	= ""
-ACCESS_TOKEN	= ""
-ACCESS_SECRET	= ""
+CONSUMER_KEY    = ""
+CONSUMER_SECRET = ""
+ACCESS_TOKEN    = ""
+ACCESS_SECRET   = ""
 
 # directory to store retrieved user timelines into
 DIRECTORY = os.path.join( '..', '..', 'data' )
