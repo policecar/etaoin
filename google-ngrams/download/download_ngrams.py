59afbd9f11964b956d8481ca7e8692b029ee15d4
Priska Herger <priska@23bit.net>
2013-11-14 17:05:21 +0100

    [minor] converted indentation to spaces.

--- google-ngrams/download/download_ngrams.py
+++ google-ngrams/download/download_ngrams.py
@@ -16,42 +16,42 @@ import urllib
 
 # from IPython import embed
 
-version 	= "20120701"
-language 	= "eng-all"
+version     = "20120701"
+language    = "eng-all"
 # languages: eng-all, eng-us-all, eng-gb-all, fre-all, ger-all, heb-all, ita-all, rus-all, spa-all
 
-base_url 	= "http://storage.googleapis.com/books/ngrams/books/"
-index_page 	= "datasetsv2.html"
-pattern		= r"googlebooks-%s-([\d\w]+)-%s" % ( language, version )
+base_url    = "http://storage.googleapis.com/books/ngrams/books/"
+index_page  = "datasetsv2.html"
+pattern     = r"googlebooks-%s-([\d\w]+)-%s" % ( language, version )
 
 # scrape relevant links from the landing /index page
 http = httplib2.Http()
 status, response = http.request( base_url + index_page )
 
 for link in BeautifulSoup( response, parseOnlyThese=SoupStrainer('a') ):
-	
-	if link.has_key( 'href' ):
-		url = link['href']
-		
-		# if url matches specified language and version
-		match = re.search( pattern, url )
-		if match:
+    
+    if link.has_key( 'href' ):
+        url = link['href']
+        
+        # if url matches specified language and version
+        match = re.search( pattern, url )
+        if match:
 
-			filename = os.path.basename( url )
-			whatgram = match.group(1)			
-			subdir = os.path.join( language, whatgram )
-			
-			# if directory doesn't exist, make it
-			if not os.path.exists( language ):	os.mkdir( language )
-			if not os.path.exists( subdir ): 	os.mkdir( subdir )
-			
-			# if file doesn't exist, download it
-			#TODO: add checksum /hash test or some other way to verify file integrity
-			filepath = os.path.join( language, whatgram, filename )
-			if not os.path.exists( filepath ):
-				try:
-					print "# Downloading %s to %s" % ( url, filepath )
-					urllib.urlretrieve( url, filepath )
-				except urllib.ContentTooShortError:
-					os.remove( filepath )
-					urllib.urlretrieve( url, filepath )
+            filename = os.path.basename( url )
+            whatgram = match.group(1)           
+            subdir = os.path.join( language, whatgram )
+            
+            # if directory doesn't exist, make it
+            if not os.path.exists( language ):  os.mkdir( language )
+            if not os.path.exists( subdir ):    os.mkdir( subdir )
+            
+            # if file doesn't exist, download it
+            #TODO: add checksum /hash test or some other way to verify file integrity
+            filepath = os.path.join( language, whatgram, filename )
+            if not os.path.exists( filepath ):
+                try:
+                    print "# Downloading %s to %s" % ( url, filepath )
+                    urllib.urlretrieve( url, filepath )
+                except urllib.ContentTooShortError:
+                    os.remove( filepath )
+                    urllib.urlretrieve( url, filepath )
